3680ffa2613b7d20c428eb44d569bb0bd3ca4bf6
JaoNotLitz <joaoholanda132005@gmail.com>
2024-07-12 14:07:09 -0300

    almost full working the version 1

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 class Program{
+    private static string[] tabuleiro = new string[]{"0","1","2","3","4","5","6","7","8"};
+
     public static void Main(string[] args){
         bool[] jx = new bool[]{false,false,false,false,false,false,false,false,false};
         bool[] jy = new bool[]{false,false,false,false,false,false,false,false,false};
-        string[] tabuleiro = new string[]{"-","-","-","-","-","-","-","-","-"};
         bool winx=false;
         bool winy=false;
         while(!winx||!winy){
             int jogada;
             PrintTable(tabuleiro);
-            Console.WriteLine();
+            if(IsWinner(jy)){
+                winy=true;
+                Console.WriteLine("O ganhador é 'Y'");
+                break;
+            }
+            else{
 
             Console.WriteLine("X, efetue sua jogada.");
             try{
@@ -18,12 +24,25 @@ class Program{
             catch(Exception e){
                 throw new Exception("Jogo encerrado, mexer dps nisso");
             }
-            jx[jogada] = true;
-
-
-
+            ColocaPonto(jogada,jx,jy,"X");
+            
+            PrintTable(tabuleiro);
+            if(IsWinner(jx)){
+                winx=true;
+                Console.WriteLine("O ganhador é 'X'");
+                break;
+            }
+            else{
             Console.WriteLine("Y, efetue sua jogada.");
-            Console.ReadLine();
+            try{
+            jogada = int.Parse(Console.ReadLine());
+            }
+            catch(Exception e){
+                throw new Exception("Jogo encerrado, mexer dps nisso");
+            }
+            ColocaPonto(jogada,jy,jx,"Y");
+                }
+            }
         }
 
     }
@@ -34,20 +53,41 @@ class Program{
                 }
                 Console.Write("|"+tabuleiro[i]);
             }
+            Console.WriteLine();
     }
+
+
+    public static void ColocaPonto(int position,bool[] jogador,bool[] adversario,string desenho){
+        if(jogador[position]||adversario[position]||position<0||position>8){
+            Console.WriteLine("A posição no tabuleiro esta ocupada por outra peça ou a jogada é inválida, jogue denovo");
+            try{
+            position = int.Parse(Console.ReadLine());
+            }
+            catch(Exception e){
+                throw new Exception("Jogo encerrado, mexer dps nisso");
+            }
+            ColocaPonto(position,jogador,adversario,desenho);
+        }
+        else{
+            jogador[position]=true;
+            tabuleiro[position]=desenho;
+        }
+    }
+
+
     public static bool IsWinner(bool[] var){
-               if(
-            var[0]&&var[1]&&var[3]||
-            var[4]&&var[5]&&var[6]||
-            var[7]&&var[8]&&var[9]||
-            
-            var[0]&&var[4]&&var[7]||
-            var[1]&&var[5]&&var[8]||
-            var[3]&&var[6]&&var[9]||
-            
-            var[0]&&var[5]&&var[9]||
-            var[3]&&var[5]&&var[7]
-            ){
+               if (
+    var[0] && var[1] && var[2] ||
+    var[3] && var[4] && var[5] ||
+    var[6] && var[7] && var[8] ||
+    
+    var[0] && var[3] && var[6] ||
+    var[1] && var[4] && var[7] ||
+    var[2] && var[5] && var[8] ||
+    
+    var[0] && var[4] && var[8] ||
+    var[2] && var[4] && var[6]
+){
 
             return true;
         }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8307ac8ff49d8d7ac6221c309abadccf22b14b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
